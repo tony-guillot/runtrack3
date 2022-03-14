a515469e90532b2b90699919bee63326d2f1090d
tony-guillot <tony.guilot@laplateforme.io>
2022-03-14 17:08:17 +0100

    job 08 terminé

--- jour01/job08/script.js
+++ jour01/job08/script.js
@@ -2,9 +2,25 @@
 function sommenombrespremiers(premiers1, premiers2){
 
 
-    
+    if(premiers1 && premiers2 < 2){
 
+        return false
+    }
 
+    for(i=2; i<= premiers1 && premiers2 ; i++){
 
+        if( premiers1 && premiers2 % i == 0){
+
+            console.log('ce ne sont pas des nombres premier')
+
+        }
+
+      else{
+
+        console.log(premiers1,premiers2)
+      }
+    }
 
 }
+
+console.log(sommenombrespremiers(3,5))
