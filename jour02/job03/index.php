7d746ad5ce64e08a4d1127fbcd1c7ae46c37ed03
tony-guillot <tony.guilot@laplateforme.io>
2022-03-15 15:05:34 +0100

    add reset button

---
+++ jour02/job03/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="script.js"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    
+    <p id="compteur">0</p>
+
+
+    <button id="button">ajouter 1 </button>
+    <button id="reset">reset </button>
+</body>
+
+</html>
---
+++ jour02/job03/script.js
@@ -0,0 +1,27 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+   function addone(){
+
+       
+       var count = 0
+    let p = document.getElementById('compteur')
+
+     let btn = document.getElementById('button').onclick=function(){
+        count =count+1;
+        p.innerHTML=count}
+
+       let reset = document.getElementById('reset').onclick=function(){
+            
+            count = 0
+
+            p.innerHTML=count
+
+       }
+
+     
+
+    }
+
+    addone()
+})
