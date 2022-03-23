2a1530548b224f029ed7dc93aefbc28f8488adc2
tony-guillot <tony.guilot@laplateforme.io>
2022-03-23 12:09:10 +0100

    jour 08 job 01

---
+++ jour08/job01/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="">Acceil</a></li>
+          <li><a href="">Inscription</a></li>
+          <li><a href="">Connexion</a></li>
+          <li><a href="">Recheche</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <form action="">
+      <input type="radio" value="Homme">
+      <label for="Homme">Homme</label>
+
+      <input type="radio" value="Femme">
+      <label for="Femme">Femme</label>
+
+      <input type="text" placeholder="Votre nom">
+      <input type="text" placeholder="votre prénom">
+      <input type="text" placeholder="votre adresse ">
+
+      <input type="checkbox">
+    </form>
+
+  
+</body>
+</html>
